595898c9afb8b68729ed65531e59a22b9fa4b644
kevin <iamforeverme@163.com>
2016-06-01 23:47:15 +0800

    Bug fix for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 docker build -t iamforeverme/python2:1 crawls
 docker-compose build
+postgresDB=$(docker-compose run -d postgresDb)
+sleep 15s
 docker-compose run web python manage.py migrate
+docker rm -f $postgresDB
